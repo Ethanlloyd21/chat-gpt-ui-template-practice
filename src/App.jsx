ffc43e11d767f83829ecb052f23befc41391043e
Lloyd Marcelino <lloydmarcelino@gmail.com>
2024-04-14 17:36:33 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import Chat from "./pages/Chat";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import useStore from "./store/useStore.jsx";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const { appTheme } = useStore((s) => s);
-
-  const darkTheme = createTheme({
-    palette: {
-      mode: appTheme,
-    },
-  });
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Chat />
-      </ThemeProvider>
-    </>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   );
 }
 
